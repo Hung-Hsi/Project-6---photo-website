64eb3ec06a30bead905468d191bb28afc893ff6f
Jogle Chang <jogle chang@gmail.com>
2023-07-03 01:14:19 +0800

    123213

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import { Routes, Route } from "react-router-dom";
+import "./styles/css/style.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Nav />
+      {/* <Homepage /> */}
+      <Routes>
+        <Route path="/photo-website" element={<Homepage />} />
+        <Route path="/photo-website/about" element={<About />} />
+      </Routes>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+//# 從 "react-dom" 中引入 createRoot，這種方式已不再支援。
+//# 相反，你應該從 "react-dom/client" 中引入它
+// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//@  React 18  ReactDOM.render 方法不再被支援 ，改用 createRoot 方法
+const rootElement = document.getElementById('root');
+
+// 使用 createRoot 方法來渲染應用程式的根元件
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
